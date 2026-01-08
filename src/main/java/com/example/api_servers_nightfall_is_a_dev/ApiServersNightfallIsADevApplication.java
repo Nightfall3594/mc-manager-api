85c8d556fd1f11a821206e2e0c056bb5b7d8029c
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-08 21:21:08 +0800

    chore: initialize project

---
+++ src/main/java/com/example/api_servers_nightfall_is_a_dev/ApiServersNightfallIsADevApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_servers_nightfall_is_a_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiServersNightfallIsADevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiServersNightfallIsADevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/api_servers_nightfall_is_a_dev/ApiServersNightfallIsADevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.api_servers_nightfall_is_a_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServersNightfallIsADevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
