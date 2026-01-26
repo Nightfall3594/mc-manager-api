1d4e24a07d5316cae5f8cbddcb1fa16714f4561f
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-26 17:22:33 +0800

    feat: restrict backup download when server is open
    important to prevent data corruption

--- src/main/java/com/example/api_servers_nightfall_is_a_dev/Backups/BackupController.java
+++ src/main/java/com/example/api_servers_nightfall_is_a_dev/Backups/BackupController.java
@@ -1,15 +1,12 @@
 package com.example.api_servers_nightfall_is_a_dev.Backups;
 
+import com.example.api_servers_nightfall_is_a_dev.common.ServerStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
-
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,8 +15,17 @@ public class BackupController {
     @Autowired
     private final BackupService backupService;
 
+    @Autowired
+    private final ServerStatus serverStatus;
+
     @GetMapping("/world/backup")
     public ResponseEntity<StreamingResponseBody> getBackup(){
+
+        if (!serverStatus.isOnline()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(null);
+        }
 
         StreamingResponseBody body = backupService::getBackup;
         return ResponseEntity.ok()
