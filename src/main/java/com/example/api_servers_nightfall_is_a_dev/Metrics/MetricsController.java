9896bd3dc30c7b578ca48a0b0c20629807b4586a
Nightfall3594 <briankristoferperez28@gmail.com>
2026-01-30 14:33:28 +0800

    refactor(metrics): migrate controller constructor to lombok annotation

--- src/main/java/com/example/api_servers_nightfall_is_a_dev/Metrics/MetricsController.java
+++ src/main/java/com/example/api_servers_nightfall_is_a_dev/Metrics/MetricsController.java
@@ -1,15 +1,14 @@
 package com.example.api_servers_nightfall_is_a_dev.Metrics;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class MetricsController {
 
     @Autowired
     private final MetricsService metricsService;
 
-    public MetricsController(MetricsService metricsService){
-        this.metricsService = metricsService;
-    }
 }
