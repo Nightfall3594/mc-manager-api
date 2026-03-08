6c4565017a914b9e194a5dd8459d40894f553770
Nightfall3594 <briankristoferperez28@gmail.com>
2026-03-08 18:19:23 +0800

    feat: implement reading name from mod metadata

--- src/main/java/com/example/api_servers_nightfall_is_a_dev/Mods/ModService.java
+++ src/main/java/com/example/api_servers_nightfall_is_a_dev/Mods/ModService.java
@@ -1,14 +1,21 @@
 package com.example.api_servers_nightfall_is_a_dev.Mods;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.moandjiezana.toml.Toml;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import java.util.stream.Stream;
 
 @Service
@@ -42,9 +49,42 @@ public class ModService {
     }
 
     private String getModName(Path modFile) {
-        // TODO: Get mod name from file metadata if possible
-        //  File name for now :)
-        return modFile.getFileName().toString();
+
+        try(JarFile jarFile = new JarFile(modFile.toFile())) {
+
+            // For fabric mod metadata
+            JarEntry fabricJarEntry = jarFile.getJarEntry("fabric.mod.json");
+            if(fabricJarEntry != null) {
+                InputStream jis = jarFile.getInputStream(fabricJarEntry);
+                Gson jsonParser = new Gson();
+                JsonObject modJson = jsonParser.fromJson(new InputStreamReader(jis), JsonObject.class);
+
+                if(modJson.has("name")) return modJson.get("name").getAsString();
+            }
+
+            // For forge mod metadata
+            JarEntry forgeJarEntry = jarFile.getJarEntry("META-INF/mods.toml");
+            if(forgeJarEntry != null) {
+                InputStream jis = jarFile.getInputStream(forgeJarEntry);
+
+                Toml tomlFile = new Toml().read(jis);
+                List<Toml> modsList = tomlFile.getTables("mods");
+
+                // note: forge mod metadata has multiple mods
+                //  only the first item contains the actual metadata
+                if (modsList != null && !modsList.isEmpty()) {
+                    Toml firstMod = modsList.getFirst();
+                    if (firstMod.contains("displayName")) {
+                        return firstMod.getString("displayName");
+                    }
+                }
+            }
+
+            return getFilename(modFile);
+
+        } catch (IOException e) {
+            return getFilename(modFile);
+        }
     }
 
     private String getModVersion(Path modFile) {
